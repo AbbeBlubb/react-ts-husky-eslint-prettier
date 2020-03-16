eaccbe8970bde64a2e92651a56bef932886c7b98
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2020-03-16 23:13:18 +0100

    Running ESLint and Prettier

--- src/components/ComponentOne.tsx
+++ src/components/ComponentOne.tsx
@@ -1,17 +1,5 @@
-import * as React from 'react';
+import * as React from 'react'
 
-export const ComponentOne = (): number => {
-var hello = 'hello'
-return <div>Greetings</div>;
-};
-    
-
-export class ComponentTwo extends React.Component {
-    state={ hello: null }
-
-    render() {
-        return(
-            <p>Text</p>
-        )
-    }
+export const ComponentOne = () => {
+    return <h1>Greetings</h1>
 }
